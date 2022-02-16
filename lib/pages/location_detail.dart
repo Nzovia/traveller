bad56dd5f0e61e450f3c9e3bcb2dc5be064da369
Nicholas Maundu <nickiemaundu19@gmail.com>
2022-02-16 21:19:22 +0300

    updated maps page

--- lib/pages/location_detail.dart
+++ lib/pages/location_detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class PlaceDetails extends StatefulWidget {
   const PlaceDetails({Key? key}) : super(key: key);
@@ -8,14 +9,22 @@ class PlaceDetails extends StatefulWidget {
 }
 
 class _PlaceDetailsState extends State<PlaceDetails> {
+
+  //declarations or maps variables
+  static const _initialCameraPosition = CameraPosition(
+  target: LatLng(-1.286389, 36.817223),
+  zoom: 11.0);
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Location Details",
-        style: TextStyle(fontSize: 36,
-            fontWeight: FontWeight.w500),
-      ),
-
+    return const Scaffold(
+      body: Padding(padding: EdgeInsets.fromLTRB(0.0, 16.0, 0.0, 0.0),
+       child: GoogleMap(
+            myLocationButtonEnabled: false,
+            zoomControlsEnabled: true,
+            initialCameraPosition: _initialCameraPosition),
+      )
     );
+
   }
 }

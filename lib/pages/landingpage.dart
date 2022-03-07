1ee2d05e9e1a9859d413fae6abcfd4f1cbf718b7
Nicholas Maundu <nickiemaundu19@gmail.com>
2022-03-07 23:54:09 +0300

    added a navigation drawer

--- lib/pages/landingpage.dart
+++ lib/pages/landingpage.dart
@@ -6,6 +6,7 @@ import 'package:traveller/widgets/TextsWidgets/smallerTexts.dart';
 import 'package:traveller/widgets/buttonWidget.dart';
 import 'package:traveller/widgets/cards/categorycard.dart';
 import 'package:traveller/widgets/cards/recommended_card.dart';
+import 'package:traveller/widgets/navdrawer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -33,11 +34,15 @@ class _HomePageState extends State<HomePage> {
                         width: 50,
                         height: 50,
                         color: Colors.grey,
-                        child: const Icon(
-                          Icons.menu,
-                          size: 24,
-                          color: Colors.black,
-                        ),
+                        child:GestureDetector(
+                          child: const Icon(
+                            Icons.menu,
+                            size: 24,
+                            color: Colors.black,
+                          ),
+                          onTap: () => const NavDrawer(),
+                        )
+
                       )),
                   const SizedBox(
                     width: 180,
---
+++ lib/widgets/navdrawer.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class NavDrawer extends StatelessWidget {
+  const NavDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(
+        child: Column(
+
+
+        ),
+      ),
+
+    );
+  }
+}
